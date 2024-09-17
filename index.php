6609940dc9da853c01a1a2b7c1c1c5bfaf3aaa89
Matt <playanhlapo@gmail.com>
2024-09-17 22:34:45 +0200

    First initial commit

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usapholwethu Trading</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <div>
+            
+            <div class="mobile-menu hide-desktop">
+                <i class="fas fa-bars"></i> <!-- Font Awesome bars icon for the menu -->
+            </div>
+            <nav id="" class="desktop-menu hide-mobile">
+            <!-- <h1 class="hide-mobile">USAPHOLWETHU TRADING</h1> -->
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+                <a href="#">            </a>
+            </nav>
+            <h1 class="heading-one fa fa-big">USAPHOLWETHU TRADING ENTERPRISES</h1>
+            <p id="excerpt" class="" >
+                safe and cost-effective rope access 
+                and other building services to commercial and industrial clients
+            </p>
+            <br/><br/>
+            <p id="excerpt" class="excerpt-two" >
+                <i class="fas fa-map-location-dot"></i><br/>
+                445 Pongola drive<br/>
+                Birchleigh North<br/>
+                Kempton Park<br/>
+                1618
+            </p>
+            <div class="down-arrow-container">
+                <div class="down-arrow hide-desktop">
+                    <i class="fas fa-chevron-down"></i> <!-- Font Awesome arrow icon for the menu -->
+                </div>
+            </div>
+
+        </div>
+    </header>
+    <main>
+
+        <!--Page about section-->
+        <div id="" class="section-devide about-section portion-section">
+            <div style="display:block;width:inherit;">
+                <h4 class="subheading center-text about-icon">
+                    <div><i class="fas fa-pen-nib"></i></div>
+                </h4>
+                
+                <h2 class="subheading center-text">ABOUT US</h2>
+
+                <p class="about-text center-text">Usapholwethu Trading Enterprises is a rope access company that also offers other 
+                    building related services to its customers. <br/><br/>
+                    Our qualified staff and employees have enabled us to provide the best possible services while offering finest solutions where attention is given to quality and detail resulting in satisfied clients with whom we have 
+                    lasting business relationships. <br/><br/>
+                    The foundation of our business is integrity and honesty. <br/>
+                    We take pride in client relationships that have developed resulting in interdependency and mutual growth.</p>
+            </div>
+            <div id="" class="deco-icons hide-mobile" style="">
+                <div class="deco-icon"><i class="fas fa-wrench"></i></div>
+                <div class="deco-icon"><i class="fas fa-toolbox"></i></div>
+                <div class="deco-icon"><i class="fas fa-hammer"></i></div>
+                <div class="deco-icon hide-mobile"><i class="fas fa-paint-roller "></i></div>
+                <div class="deco-icon hide-mobile"><i class="fas fa-bucket"></i></div>
+            </div>
+            <div style="display:block;width:100%;">
+                <h4 class="subheading center-text about-icon">
+                    <div><i class="fas fa-briefcase"></i></div>
+                </h4>
+                
+                <h2 class="subheading center-text">VISION</h2>
+                
+                <p class="about-text center-text">
+                    Our vision is based on the principle of “RELIABLE PERFECTION”.
+                    <br/> 
+                    Briefly, to provide our 
+                    Clients with the finest solutions, on time, every time.
+                </p>
+            </div>
+        </div>
+
+         <!--Page about section-->
+        <div id="" class="core-values">
+            <h4> ---- why choose <span>usapholwethu</span></h4>
+            <div>
+            <div id="" class="">
+                <h4>
+                    <i class="fas fa-clock"></i>
+                    Inovative
+                </h4>
+                <div id="" class="">
+                    We pride ourselves in our innovative ways of working and 
+                    finding solutions that are modern day working techniques.
+                </div>
+            </div>
+            <div id="" class="">
+                <h4>
+                    <i class="fas fa-lock"></i>
+                    Secure
+                </h4>
+                <div id="" class="">
+                    Our innovative way of thinking allows us the luxury of
+                    working in the best possible safe working environment
+                    giving our workers guarantee of safety.
+                </div>
+            </div>
+            <div id="" class="">
+                <h4>
+                    <i class="fas fa-dollar"></i>
+                    Money Saving
+                </h4>
+                <div id="" class="">
+                    Our safe and innovative working practices are guaranteed
+                     to save you money because our solutions are everlasting.
+                </div>
+            </div>
+
+            </div>
+        </div>
+
+        <!--Why choose us section-->
+        <div id="" class="section-devide special-section portion-section">
+            <!-- <div style="display:block;width:inherit;" class="">
+                <h4 class="subheading center-text about-icon">
+                    <i class="fas fa-users"></i>
+                </h4>
+                <h2 class="subheading center-text"></h2>
+            </div> -->
+
+            <!--People to contact-->
+            <div class="people">
+
+                <!--Single contact card-->
+                <div class="person person-left">
+                    <div id="" class="person-info person-under">
+                        <div>Nompumelelo Shabangu</div>
+                        <div>Administrator</div>
+                        <div>mpumi@usapholwethutrading.co.za</div>
+                        <div>+27 079 951 6293</div>
+                    </div>
+                    <div id="" class="person-image person-under" style="background-image: url('images/profile.jpg');">
+                    </div>
+                </div>
+
+                <!--Single contact card-->
+                <div class="person person-right">
+                    <div id="" class="person-info person-under">
+                        <div>Mzwandile Ntanzi</div>
+                        <div>Managing Director</div>
+                        <div>mzwandile@usapholwethutrading.co.za</div>
+                        <div>+27 084 584 9511</div>
+                    </div>
+                    <div id="" class="person-image person-under" style="background-image: url('images/profile1.jpg');">
+                    </div>
+                </div>
+
+                <!--Single contact card-->
+                <!-- <div class="person person-left">
+                    <div id="" class="person-info person-under">
+                        <div>Matt Obose</div>
+                        <div>065 872 9073</div>
+                        <div>matt.obose@moov.life</div>
+                        <div>CEO</div>
+                    </div>
+                    <div id="" class="person-image person-under">
+                        <i class="fas fa-user"></i>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+
+        <!-- Services section-->
+        <div id="" class="section-devide services-section portion-section services-card">
+            <div style="display:block;width:inherit;" class="">
+                <h2 class="subheading center-text">SERVICES</h2>
+                <br/>
+                <h4 class="subheading center-text services-icon">
+                    <div><i class="fas fa-helmet-safety"></i></div>
+                </h4>
+                <h4 class="subheading center-text">CONSTRUCTION</h4>
+                <ul>
+                    <li>- Bricklaying & Plastering -</li>
+                    <li>- Paving -</li>
+                    <li>- Tiling -</li>
+                    <li>- Torch on Waterprofing -</li>
+                    <li>- Screeding -</li>
+                    <li>- Ceilings -</li>
+                    <li>- Painting -</li>
+                </ul>
+            </div>
+            <br/>
+            <div style="display:block;width:100%;">
+                <h4 class="subheading center-text services-icon">
+                    <div><i class="fas fa-screwdriver-wrench"></i></div>
+                </h4>
+                <h4 class="subheading center-text">SERVICES</h4>
+                <ul>
+                    <li>- Silo cleaning -</li>
+                    <li>- Sheeting -</li>
+                    <li>- Bolting -</li>
+                    <li>- Painting -</li>
+                    <li>- Signage Installation -</li>
+                    <li>- Boiler Maintanance -</li>
+                    <li>- Waterproofing -</li>
+                    <li>- Concrete repairs -</li>
+                    <li>- Building maintenance -</li>
+                    <li>- High Pressure Cleaning -</li>
+                    <li>- Window Washing -</li>
+                    <li>- Glass Installation -</li>
+                    <li>- Life Line Installation -</li>
+                    <li>- Mining -</li>
+                    <li>- Plant Cleaning -</li>
+                    <li>- Joint Sealing -</li>
+                    <li>- Window Sealing -</li>
+                    <li>- Inspections -</li>
+                </ul>
+            </div>
+        </div><!--Close services section-->
+
+        <div id="" class="section-devide gallery-section portion-section">
+            <div style="display:block;width:inherit;" class="image-slider">
+                
+            <div class="image-card"><!--One image with short description-->
+                <div class="squared-image" style="background-image: url('images/back (1).jpg');">
+                    <div id="spacer">
+                    </div>
+                    <section id="image-description" class="" >
+                        <h5 >We Pride ourselves in our previus achievements and even future projects to come </h5>
+                    </section>
+                </div>
+            </div>
+
+                
+            <div class="image-card"><!--One image with short description-->
+                <div class="squared-image" style="background-image: url('images/image (1).jpg');">
+                    <div id="spacer">
+                    </div>
+                    <section id="image-description" class="" >
+                        <h5>We Pride ourselves in our previus achievements and even future projects to come </h5>
+                    </section>
+                </div>
+            </div>
+                            
+            <div class="image-card"><!--One image with short description-->
+                <div class="squared-image" style="background-image: url('images/image (2).jpg');">
+                    <div id="spacer">
+                    </div>
+                    <section id="image-description" class="">
+                        <h5>We Pride ourselves in our previus achievements and even future projects to come </h5>
+                    </section>
+                </div>
+            </div>
+
+                            
+            <div class="image-card"><!--One image with short description-->
+                <div class="squared-image" style="background-image: url('images/image (3).jpg');">
+                    <div id="spacer">
+                    </div>
+                    <section id="image-description" class="">
+                        <h5>We Pride ourselves in our previus achievements and even future projects to come </h5>
+                    </section>
+                </div>
+            </div>
+                            
+            <div class="image-card"><!--One image with short description-->
+                <div class="squared-image" style="background-image: url('images/image (12).jpg');">
+                    <div id="spacer">
+                    </div>
+                    <section id="image-description" class="">
+                        <h5>We Pride ourselves in our previus achievements and even future projects to come </h5>
+                    </section>
+                </div>
+            </div>
+
+            <div class="image-card"><!--One image with short description-->
+                <div class="squared-image" style="background-image: url('images/image (6).jpg');    background-position: bottom !important;">
+                    <div id="spacer">
+                    </div>
+                    <section id="image-description" class="">
+                        <h5>We Pride ourselves in our previus achievements and even future projects to come </h5>
+                    </section>
+                </div>
+            </div>
+
+            <div class="image-card"><!--One image with short description-->
+                <div class="squared-image" style="background-image: url('images/image (7).jpg');">
+                    <div id="spacer">
+                    </div>
+                    <section id="image-description" class="">
+                        <h5>We Pride ourselves in our previus achievements and even future projects to come </h5>
+                    </section>
+                </div>
+            </div>
+
+            <div class="image-card"><!--One image with short description-->
+                <div class="squared-image" style="background-image: url('images/image (10).jpg');">
+                    <div id="spacer">
+                    </div>
+                    <section id="image-description" class="">
+                        <h5>We Pride ourselves in our previus achievements and even future projects to come </h5>
+                    </section>
+                </div>
+            </div>
+
+            <div class="image-card"><!--One image with short description-->
+                <div class="squared-image" style="background-image: url('images/image (9).jpg');">
+                    <div id="spacer">
+                    </div>
+                    <section id="image-description" class="">
+                        <h5>We Pride ourselves in our previus achievements and even future projects to come </h5>
+                    </section>
+                </div>
+            </div>
+            </div> <!-- close image slider-->
+        </div><!--close image slider container-->
+
+        <!--Some of the Companies we have worked with-->
+        <div id="" class="section-devide contact-section portion-section">
+            <div style="display:block;width:inherit;" class="">
+                <h4 class="subheading center-text about-icon">
+                    <i class="fas fa-phone"></i>
+                </h4>
+                <h2 class="subheading center-text">GET IN TOUCH</h2>
+                <form action="" method="post">
+                    <input type="text" name="name" id="" placeholder="Individual/Company name">
+                    <input type="text" name="email" id="" placeholder="info@example.com">
+                    <input type="text" name="request" id="" placeholder="Your message">
+                    <br/>
+                    <input type="submit" value="send">
+                </form>
+            </div>
+            <div style="display:block;width:inherit;" class="social-links">
+                <p>
+                    click on any of those to get in touch on to get in touch
+                </p>
+                <br/>
+                <a target="orphan" href="https://www.facebook.com/usaphoent" id="" class=""><i class="fa fa-facebook-square"></i></a>
+                <a target="orphan" href="https://www.instagram.com/usapholwethutrading?igsh=MzRIODBiNWFIZA==" id="" class=""><i class="fa fa-instagram"></i></a>
+                <a target="orphan"  href="https://wa.me/27799516293" id="" class=""><i class="fa fa-whatsapp"></i></a>
+
+            </div>
+        </div>
+    </main>
+    
+    <footer>
+        <p>&copy; 2024 All rights reserved.</p>
+        <p><i class="fas fa-mail"></i>info@usapholwethutrading.co.za</p>
+    </footer>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
